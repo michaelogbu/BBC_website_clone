bd64968325334bcb242d2b852fa551ad0e0f2792
ogbumichael <michaelemekaogbu@gmail.com>
2023-07-16 02:20:09 -0500

    javaScript in navigation now working

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-const navTwo = document.querySelector(".navTwo")
+// NAVIGATION
+const navContainer = document.querySelector(".navBootomContainer")
+const closeMenu = document.querySelector(".times")
+const showMenu = document.querySelectorAll(".bars")
 
 
-console.log(navTwo);
+// console.log(navContainer.parentElement);
 
+const menuShow = function(){
+    const active = document.querySelector('.activeNav')
+    const bottomNavParent = navContainer.parentElement
+    // console.log(bottomNavParent);
+    if (!bottomNavParent.classList.contains('activeNav')) {
+        bottomNavParent.classList.add('activeNav')
+    } else {
+        bottomNavParent.classList.remove('activeNav')
+    }
+}
 
+// menuShow()
+showMenu.forEach((Btn) => {
+    Btn.addEventListener('click', (e) => {
+        const btn = e.target
+        // console.log(btn);
+        if (btn.classList.contains('dots')) {
+            menuShow()
+            // console.log(123);
+        }
+        if (btn.classList.contains('bar')) {
+            menuShow()
+        }
+    })
+})
 
